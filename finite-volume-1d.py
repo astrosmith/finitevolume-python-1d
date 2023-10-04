ec972544d3e12b658715376819b3f2d7b0c2340f
Aaron Smith <asmith@astro.as.utexas.edu>
2023-10-04 12:10:55 -0500

    sod test

--- finite-volume-1d.py
+++ finite-volume-1d.py
@@ -9,265 +9,266 @@ Simulate the Kelvin Helmholtz Instability
 In the compressible Euler equations
 
 """
-# to do remove y
 
-def getConserved( rho, vx, vy, P, gamma, vol ):
-	"""
+def getConserved(rho, v, P, gamma, vol):
+    """
     Calculate the conserved variable from the primitive
-	rho      is matrix of cell densities
-	vx       is matrix of cell x-velocity
-	vy       is matrix of cell y-velocity
-	P        is matrix of cell pressures
-	gamma    is ideal gas gamma
-	vol      is cell volume
-	Mass     is matrix of mass in cells
-	Momx     is matrix of x-momentum in cells
-	Energy   is matrix of energy in cells
-	"""
-	Mass   = rho * vol
-	Momx   = rho * vx * vol
-	Energy = (P/(gamma-1) + 0.5*rho*(vx**2))*vol
-	
-	return Mass, Momx, Energy
+    rho      is matrix of cell densities
+    v        is matrix of cell velocity
+    P        is matrix of cell pressures
+    gamma    is ideal gas gamma
+    vol      is cell volume
+    Mass     is matrix of mass in cells
+    Momemtum is matrix of momentum in cells
+    Energy   is matrix of energy in cells
+    """
+    Mass   = rho * vol
+    Momemtum   = rho * v * vol
+    Energy = (P/(gamma-1) + 0.5*rho*v**2) * vol
+
+    return Mass, Momemtum, Energy
 
 
-def getPrimitive( Mass, Momx, Energy, gamma, vol ):
-	"""
+def getPrimitive(Mass, Momemtum, Energy, gamma, vol):
+    """
     Calculate the primitive variable from the conservative
-	Mass     is matrix of mass in cells
-	Momx     is matrix of x-momentum in cells
-	Energy   is matrix of energy in cells
-	gamma    is ideal gas gamma
-	vol      is cell volume
-	rho      is matrix of cell densities
-	vx       is matrix of cell x-velocity
-	P        is matrix of cell pressures
-	"""
-	rho = Mass / vol
-	vx  = Momx / rho / vol
-	P   = (Energy/vol - 0.5*rho * (vx**2)) * (gamma-1)
-	
-	return rho, vx, P
+    Mass     is matrix of mass in cells
+    Momemtum is matrix of momentum in cells
+    Energy   is matrix of energy in cells
+    gamma    is ideal gas gamma
+    vol      is cell volume
+    rho      is matrix of cell densities
+    v        is matrix of cell velocity
+    P        is matrix of cell pressures
+    """
+    rho = Mass / vol
+    v  = Momemtum / rho / vol
+    P   = (Energy/vol - 0.5*rho * v**2) * (gamma-1)
+
+    return rho, v, P
 
 
 def getGradient(f, dx):
-	"""
+    """
     Calculate the gradients of a field
-	f        is a matrix of the field
-	dx       is the cell size
-	f_dx     is a matrix of derivative of f in the x-direction
-	"""
-	# directions for np.roll() 
-	R = -1   # right
-	L = 1    # left
-	
-	f_dx = ( np.roll(f,R,axis=0) - np.roll(f,L,axis=0) ) / (2*dx)
-	
-	return f_dx
+    f        is a matrix of the field
+    dx       is the cell size
+    f_dx     is a matrix of derivative of f in the x-direction
+    """
+    # directions for np.roll()
+    R = -1   # right
+    L = 1    # left
+
+    f_dx = (np.roll(f,R) - np.roll(f,L)) / (2*dx)
+
+    return f_dx
 
 
 def slopeLimit(f, dx, f_dx):
-	"""
+    """
     Apply slope limiter to slopes
-	f        is a matrix of the field
-	dx       is the cell size
-	f_dx     is a matrix of derivative of f in the x-direction
-	"""
-	# directions for np.roll() 
-	R = -1   # right
-	L = 1    # left
-	
-	f_dx = np.maximum(0., np.minimum(1., ( (f-np.roll(f,L,axis=0))/dx)/(f_dx + 1.0e-8*(f_dx==0)))) * f_dx
-	f_dx = np.maximum(0., np.minimum(1., (-(f-np.roll(f,R,axis=0))/dx)/(f_dx + 1.0e-8*(f_dx==0)))) * f_dx
-	
-	return f_dx
+    f        is a matrix of the field
+    dx       is the cell size
+    f_dx     is a matrix of derivative of f in the x-direction
+    """
+    # directions for np.roll()
+    R = -1   # right
+    L = 1    # left
+
+    f_dx = np.maximum(0., np.minimum(1., ((f-np.roll(f,L))/dx)/(f_dx + 1.0e-8*(f_dx==0)))) * f_dx
+    f_dx = np.maximum(0., np.minimum(1., (-(f-np.roll(f,R))/dx)/(f_dx + 1.0e-8*(f_dx==0)))) * f_dx
+
+    return f_dx
 
 
 def extrapolateInSpaceToFace(f, f_dx, dx):
-	"""
+    """
     Calculate the gradients of a field
-	f        is a matrix of the field
-	f_dx     is a matrix of the field x-derivatives
-	dx       is the cell size
-	f_XL     is a matrix of spatial-extrapolated values on `left' face along x-axis 
-	f_XR     is a matrix of spatial-extrapolated values on `right' face along x-axis 
-	"""
-	# directions for np.roll() 
-	R = -1   # right
-	L = 1    # left
-	
-	f_XL = f - f_dx * dx/2
-	f_XL = np.roll(f_XL,R,axis=0)
-	f_XR = f + f_dx * dx/2
-	
-	return f_XL, f_XR
-	
+    f        is a matrix of the field
+    f_dx     is a matrix of the field x-derivatives
+    dx       is the cell size
+    f_XL     is a matrix of spatial-extrapolated values on `left' face along x-axis
+    f_XR     is a matrix of spatial-extrapolated values on `right' face along x-axis
+    """
+    # directions for np.roll()
+    R = -1   # right
+    # L = 1    # left
+
+    f_XL = f - f_dx * dx/2
+    f_XL = np.roll(f_XL,R)
+    f_XR = f + f_dx * dx/2
+
+    return f_XL, f_XR
+
 
 def applyFluxes(F, flux_F_X, dx, dt):
-	"""
+    """
     Apply fluxes to conserved variables
-	F        is a matrix of the conserved variable field
-	flux_F_X is a matrix of the x-dir fluxes
-	dx       is the cell size
-	dt       is the timestep
-	"""
-	# directions for np.roll() 
-	R = -1   # right
-	L = 1    # left
-	
-	# update solution
-	F += - dt * dx * flux_F_X
-	F +=   dt * dx * np.roll(flux_F_X,L,axis=0)
-	
-	return F
+    F        is a matrix of the conserved variable field
+    flux_F_X is a matrix of the x-dir fluxes
+    dx       is the cell size
+    dt       is the timestep
+    """
+    # directions for np.roll()
+    # R = -1   # right
+    L = 1    # left
+
+    # update solution
+    F += - dt * dx * flux_F_X
+    F +=   dt * dx * np.roll(flux_F_X,L)
+
+    return F
 
 
-def getFlux(rho_L, rho_R, vx_L, vx_R, vy_L, vy_R, P_L, P_R, gamma):
-	"""
-    Calculate fluxed between 2 states with local Lax-Friedrichs/Rusanov rule 
-	rho_L        is a matrix of left-state  density
-	rho_R        is a matrix of right-state density
-	vx_L         is a matrix of left-state  x-velocity
-	vx_R         is a matrix of right-state x-velocity
-	P_L          is a matrix of left-state  pressure
-	P_R          is a matrix of right-state pressure
-	gamma        is the ideal gas gamma
-	flux_Mass    is the matrix of mass fluxes
-	flux_Momx    is the matrix of x-momentum fluxes
-	flux_Energy  is the matrix of energy fluxes
-	"""
-	
-	# left and right energies
-	en_L = P_L/(gamma-1)+0.5*rho_L * (vx_L**2)
-	en_R = P_R/(gamma-1)+0.5*rho_R * (vx_R**2)
+def getFlux(rho_L, rho_R, v_L, v_R, P_L, P_R, gamma):
+    """
+    Calculate fluxed between 2 states with local Lax-Friedrichs/Rusanov rule
+    rho_L        is a matrix of left-state  density
+    rho_R        is a matrix of right-state density
+    v_L          is a matrix of left-state  velocity
+    v_R          is a matrix of right-state velocity
+    P_L          is a matrix of left-state  pressure
+    P_R          is a matrix of right-state pressure
+    gamma        is the ideal gas gamma
+    flux_Mass    is the matrix of mass fluxes
+    flux_Momemtum is the matrix of momentum fluxes
+    flux_Energy  is the matrix of energy fluxes
+    """
 
-	# compute star (averaged) states
-	rho_star  = 0.5*(rho_L + rho_R)
-	momx_star = 0.5*(rho_L * vx_L + rho_R * vx_R)
-	en_star   = 0.5*(en_L + en_R)
-	
-	P_star = (gamma-1)*(en_star-0.5*(momx_star**2)/rho_star)
-	
-	# compute fluxes (local Lax-Friedrichs/Rusanov)
-	flux_Mass   = momx_star
-	flux_Momx   = momx_star**2/rho_star + P_star
-	flux_Energy = (en_star+P_star) * momx_star/rho_star
-	
-	# find wavespeeds
-	C_L = np.sqrt(gamma*P_L/rho_L) + np.abs(vx_L)
-	C_R = np.sqrt(gamma*P_R/rho_R) + np.abs(vx_R)
-	C = np.maximum( C_L, C_R )
-	
-	# add stabilizing diffusive term
-	flux_Mass   -= C * 0.5 * (rho_L - rho_R)
-	flux_Momx   -= C * 0.5 * (rho_L * vx_L - rho_R * vx_R)
-	flux_Energy -= C * 0.5 * ( en_L - en_R )
+    # left and right energies
+    en_L = P_L/(gamma-1)+0.5*rho_L * (v_L**2)
+    en_R = P_R/(gamma-1)+0.5*rho_R * (v_R**2)
 
-	return flux_Mass, flux_Momx, flux_Energy
+    # compute star (averaged) states
+    rho_star  = 0.5*(rho_L + rho_R)
+    momx_star = 0.5*(rho_L * v_L + rho_R * v_R)
+    en_star   = 0.5*(en_L + en_R)
+
+    P_star = (gamma-1)*(en_star-0.5*(momx_star**2)/rho_star)
+
+    # compute fluxes (local Lax-Friedrichs/Rusanov)
+    flux_Mass   = momx_star
+    flux_Momemtum   = momx_star**2/rho_star + P_star
+    flux_Energy = (en_star+P_star) * momx_star/rho_star
+
+    # find wavespeeds
+    C_L = np.sqrt(gamma*P_L/rho_L) + np.abs(v_L)
+    C_R = np.sqrt(gamma*P_R/rho_R) + np.abs(v_R)
+    C = np.maximum(C_L, C_R)
+
+    # add stabilizing diffusive term
+    flux_Mass   -= C * 0.5 * (rho_L - rho_R)
+    flux_Momemtum -= C * 0.5 * (rho_L * v_L - rho_R * v_R)
+    flux_Energy -= C * 0.5 * (en_L - en_R)
+
+    return flux_Mass, flux_Momemtum, flux_Energy
 
 
 
 
 def main():
-	""" Finite Volume simulation """
-	
-	# Simulation parameters
-	N                      = 128 # resolution
-	boxsize                = 1.
-	gamma                  = 5/3 # ideal gas gamma
-	courant_fac            = 0.4
-	t                      = 0
-	tEnd                   = 2
-	tOut                   = 0.02 # draw frequency
-	useSlopeLimiting       = False
-	plotRealTime = True # switch on for plotting as the simulation goes along
-	
-	# Mesh
-	dx = boxsize / N
-	vol = dx**2
-	xlin = np.linspace(0.5*dx, boxsize-0.5*dx, N)
-	Y, X = np.meshgrid( xlin, xlin )
-	
-	# Generate Initial Conditions - opposite moving streams with perturbation
-	w0 = 0.1
-	sigma = 0.05/np.sqrt(2.)
-	rho = 1. + (np.abs(Y-0.5) < 0.25)
-	vx = -0.5 + (np.abs(Y-0.5)<0.25)
-	P = 2.5 * np.ones(X.shape)
+    """ Finite Volume simulation """
 
-	# Get conserved variables
-	Mass, Momx, Energy = getConserved( rho, vx, P, gamma, vol )
-	
-	# prep figure
-	fig = plt.figure(figsize=(4,4), dpi=80)
-	outputCount = 1
-	
-	# Simulation Main Loop
-	while t < tEnd:
-		
-		# get Primitive variables
-		rho, vx, P = getPrimitive( Mass, Momx, Energy, gamma, vol )
-		
-		# get time step (CFL) = dx / max signal speed
-		dt = courant_fac * np.min( dx / (np.sqrt( gamma*P/rho ) + np.sqrt(vx**2)) )
-		plotThisTurn = False
-		if t + dt > outputCount*tOut:
-			dt = outputCount*tOut - t
-			plotThisTurn = True
-		
-		# calculate gradients
-		rho_dx, rho_dy = getGradient(rho, dx)
-		vx_dx,  vx_dy  = getGradient(vx,  dx)
-		P_dx,   P_dy   = getGradient(P,   dx)
-		
-		# slope limit gradients
-		if useSlopeLimiting:
-			rho_dx, rho_dy = slopeLimit(rho, dx, rho_dx, rho_dy)
-			vx_dx,  vx_dy  = slopeLimit(vx , dx, vx_dx,  vx_dy )
-			P_dx,   P_dy   = slopeLimit(P  , dx, P_dx,   P_dy  )
-		
-		# extrapolate half-step in time
-		rho_prime = rho - 0.5*dt * ( vx * rho_dx + rho * vx_dx * rho_dy + rho)
-		vx_prime  = vx  - 0.5*dt * ( vx * vx_dx * vx_dy + (1/rho) * P_dx )
-		P_prime   = P   - 0.5*dt * ( gamma*P * (vx_dx)  + vx * P_dx * P_dy )
-		
-		# extrapolate in space to face centers
-		rho_XL, rho_XR, rho_YL, rho_YR = extrapolateInSpaceToFace(rho_prime, rho_dx, rho_dy, dx)
-		vx_XL,  vx_XR,  vx_YL,  vx_YR  = extrapolateInSpaceToFace(vx_prime,  vx_dx,  vx_dy,  dx)
-		P_XL,   P_XR,   P_YL,   P_YR   = extrapolateInSpaceToFace(P_prime,   P_dx,   P_dy,   dx)
-		
-		# compute fluxes (local Lax-Friedrichs/Rusanov)
-		flux_Mass_X, flux_Momx_X, flux_Momy_X, flux_Energy_X = getFlux(rho_XL, rho_XR, vx_XL, vx_XR, P_XL, P_XR, gamma)
-		flux_Mass_Y, flux_Momy_Y, flux_Momx_Y, flux_Energy_Y = getFlux(rho_YL, rho_YR, vx_YL, vx_YR, P_YL, P_YR, gamma)
-		
-		# update solution
-		Mass   = applyFluxes(Mass, flux_Mass_X, flux_Mass_Y, dx, dt)
-		Momx   = applyFluxes(Momx, flux_Momx_X, flux_Momx_Y, dx, dt)
-		Energy = applyFluxes(Energy, flux_Energy_X, flux_Energy_Y, dx, dt)
-		
-		# update time
-		t += dt
-		
-		# plot in real time
-		if (plotRealTime and plotThisTurn) or (t >= tEnd):
-			plt.cla()
-			plt.imshow(rho.T)
-			plt.clim(0.8, 2.2)
-			ax = plt.gca()
-			ax.invert_yaxis()
-			ax.get_xaxis().set_visible(False)
-			ax.get_yaxis().set_visible(False)	
-			ax.set_aspect('equal')	
-			plt.pause(0.001)
-			outputCount += 1
-			
-	
-	# Save figure
-	plt.savefig('finitevolume.png',dpi=240)
-	plt.show()
-	    
-	return 0
+    # Simulation parameters
+    N                      = 128 # resolution
+    boxsize                = 1.
+    gamma                  = 1.4 # 5/3 # ideal gas gamma
+    courant_fac            = 0.4
+    t                      = 0
+    tEnd                   = 0.2
+    tOut                   = 0.01 # draw frequency
+    useSlopeLimiting       = True
+    plotRealTime           = False # switch on for plotting as the simulation goes along
 
+    # Mesh
+    dx = boxsize / N
+    vol = dx
+    # x_edges = np.linspace(0., boxsize, N+1)
+    x = np.linspace(0.5*dx, boxsize-0.5*dx, N)
+
+    # Generate Initial Conditions - different values for left and right states
+    rho_L, P_L, v_L = 1., 1., 0.    # Initial state (left)
+    rho_R, P_R, v_R = .125, .1, 0.  # Initial state (right)
+    mask = x > 0.5 # Separates left/right regions
+    rho = np.empty(N); rho[~mask] = rho_L; rho[mask] = rho_R # Density
+    P = np.empty(N); P[~mask] = P_L; P[mask] = P_R # Pressure
+    v = np.empty(N); v[~mask] = v_L; v[mask] = v_R # Velocity
+
+    # Get conserved variables
+    Mass, Momemtum, Energy = getConserved(rho, v, P, gamma, vol)
+
+    # prep figure
+    fig = plt.figure(figsize=(8,6), dpi=80)
+    outputCount = 1
+
+    # if plotRealTime:
+    #     plt.cla()
+    #     plt.plot(x, rho)
+    #     plt.xlim(0., 1.)
+    #     # plt.ylim(0., 2.2)
+    #     plt.pause(3)
+    #     1/0
+
+    # Simulation Main Loop
+    while t < tEnd:
+
+        # get Primitive variables
+        rho, v, P = getPrimitive(Mass, Momemtum, Energy, gamma, vol)
+
+        # get time step (CFL) = dx / max signal speed
+        dt = courant_fac * np.min(dx / (np.sqrt(gamma*P/rho) + np.sqrt(v**2)))
+        plotThisTurn = False
+        if t + dt > outputCount*tOut:
+            dt = outputCount*tOut - t
+            plotThisTurn = True
+
+        # calculate gradients
+        rho_dx = getGradient(rho, dx)
+        v_dx = getGradient(v, dx)
+        P_dx = getGradient(P, dx)
+
+        # slope limit gradients
+        if useSlopeLimiting:
+            rho_dx = slopeLimit(rho, dx, rho_dx)
+            v_dx = slopeLimit(v, dx, v_dx)
+            P_dx = slopeLimit(P, dx, P_dx)
+
+        # extrapolate half-step in time
+        rho_prime = rho - 0.5*dt * (v * rho_dx + rho * v_dx)
+        v_prime = v - 0.5*dt * (v * v_dx + P_dx / rho)
+        P_prime = P - 0.5*dt * (gamma * P * v_dx + v * P_dx)
+
+        # extrapolate in space to face centers
+        rho_XL, rho_XR = extrapolateInSpaceToFace(rho_prime, rho_dx, dx)
+        v_XL, v_XR = extrapolateInSpaceToFace(v_prime, v_dx, dx)
+        P_XL, P_XR = extrapolateInSpaceToFace(P_prime, P_dx, dx)
+
+        # compute fluxes (local Lax-Friedrichs/Rusanov)
+        flux_Mass_X, flux_Momemtum_X, flux_Energy_X = getFlux(rho_XL, rho_XR, v_XL, v_XR, P_XL, P_XR, gamma)
+
+        # update solution
+        Mass = applyFluxes(Mass, flux_Mass_X, dx, dt)
+        Momemtum = applyFluxes(Momemtum, flux_Momemtum_X, dx, dt)
+        Energy = applyFluxes(Energy, flux_Energy_X, dx, dt)
+
+        # update time
+        t += dt
+
+        # plot in real time
+        if (plotRealTime and plotThisTurn) or (t >= tEnd):
+            plt.cla()
+            plt.plot(x, rho)
+            plt.xlim(0., 1.)
+            # plt.ylim(0., 2.2)
+            plt.title('t = %0.2f' % t)
+            plt.pause(0.001)
+            outputCount += 1
+
+    # Save figure
+    plt.savefig('density.pdf',dpi=240)
+    plt.show()
+
+    return
 
 
 if __name__== "__main__":
---
+++ sod.py
@@ -0,0 +1,236 @@
+import numpy as np
+import scipy
+import scipy.optimize
+
+
+def shock_tube_function(p4, p1, p5, rho1, rho5, gamma):
+    """
+    Shock tube equation
+    """
+    z = (p4 / p5 - 1.)
+    c1 = np.sqrt(gamma * p1 / rho1)
+    c5 = np.sqrt(gamma * p5 / rho5)
+
+    gm1 = gamma - 1.
+    gp1 = gamma + 1.
+    g2 = 2. * gamma
+
+    fact = gm1 / g2 * (c5 / c1) * z / np.sqrt(1. + gp1 / g2 * z)
+    fact = (1. - fact) ** (g2 / gm1)
+
+    return p1 * fact - p4
+
+
+def calculate_regions(pl, ul, rhol, pr, ur, rhor, gamma=1.4):
+    """
+    Compute regions
+    :rtype : tuple
+    :return: returns p, rho and u for regions 1,3,4,5 as well as the shock speed
+    """
+    # if pl > pr...
+    rho1 = rhol
+    p1 = pl
+    u1 = ul
+    rho5 = rhor
+    p5 = pr
+    u5 = ur
+
+    # unless...
+    if pl < pr:
+        rho1 = rhor
+        p1 = pr
+        u1 = ur
+        rho5 = rhol
+        p5 = pl
+        u5 = ul
+
+    # solve for post-shock pressure
+    p4 = scipy.optimize.fsolve(shock_tube_function, p1, (p1, p5, rho1, rho5, gamma))[0]
+
+    # compute post-shock density and velocity
+    z = (p4 / p5 - 1.)
+    c5 = np.sqrt(gamma * p5 / rho5)
+
+    gm1 = gamma - 1.
+    gp1 = gamma + 1.
+    gmfac1 = 0.5 * gm1 / gamma
+    gmfac2 = 0.5 * gp1 / gamma
+
+    fact = np.sqrt(1. + gmfac2 * z)
+
+    u4 = c5 * z / (gamma * fact)
+    rho4 = rho5 * (1. + gmfac2 * z) / (1. + gmfac1 * z)
+
+    # shock speed
+    w = c5 * fact
+
+    # compute values at foot of rarefaction
+    p3 = p4
+    u3 = u4
+    rho3 = rho1 * (p3 / p1)**(1. / gamma)
+    return (p1, rho1, u1), (p3, rho3, u3), (p4, rho4, u4), (p5, rho5, u5), w
+
+
+def calc_positions(pl, pr, region1, region3, w, xi, t, gamma):
+    """
+    :return: tuple of positions in the following order ->
+            Head of Rarefaction: xhd,  Foot of Rarefaction: xft,
+            Contact Discontinuity: xcd, Shock: xsh
+    """
+    p1, rho1 = region1[:2]  # don't need velocity
+    p3, rho3, u3 = region3
+    c1 = np.sqrt(gamma * p1 / rho1)
+    c3 = np.sqrt(gamma * p3 / rho3)
+    if pl > pr:
+        xsh = xi + w * t
+        xcd = xi + u3 * t
+        xft = xi + (u3 - c3) * t
+        xhd = xi - c1 * t
+    else:
+        # pr > pl
+        xsh = xi - w * t
+        xcd = xi - u3 * t
+        xft = xi - (u3 - c3) * t
+        xhd = xi + c1 * t
+
+    return xhd, xft, xcd, xsh
+
+
+def region_states(pl, pr, region1, region3, region4, region5):
+    """
+    :return: dictionary (region no.: p, rho, u), except for rarefaction region
+    where the value is a string, obviously
+    """
+    if pl > pr:
+        return {'Region 1': region1,
+                'Region 2': 'RAREFACTION',
+                'Region 3': region3,
+                'Region 4': region4,
+                'Region 5': region5}
+    else:
+        return {'Region 1': region5,
+                'Region 2': region4,
+                'Region 3': region3,
+                'Region 4': 'RAREFACTION',
+                'Region 5': region1}
+
+
+def create_arrays(pl, pr, xl, xr, positions, state1, state3, state4, state5, npts, gamma, t, xi):
+    """
+    :return: tuple of x, p, rho and u values across the domain of interest
+    """
+    xhd, xft, xcd, xsh = positions
+    p1, rho1, u1 = state1
+    p3, rho3, u3 = state3
+    p4, rho4, u4 = state4
+    p5, rho5, u5 = state5
+    gm1 = gamma - 1.
+    gp1 = gamma + 1.
+
+    # dx = (xr-xl)/(npts-1)
+    # x_arr = np.arange(xl, xr, dx)
+    x_arr = np.linspace(xl, xr, npts)
+    rho = np.zeros(npts, dtype=float)
+    p = np.zeros(npts, dtype=float)
+    u = np.zeros(npts, dtype=float)
+    c1 = np.sqrt(gamma * p1 / rho1)
+    if pl > pr:
+        for i, x in enumerate(x_arr):
+            if x < xhd:
+                rho[i] = rho1
+                p[i] = p1
+                u[i] = u1
+            elif x < xft:
+                u[i] = 2. / gp1 * (c1 + (x - xi) / t)
+                fact = 1. - 0.5 * gm1 * u[i] / c1
+                rho[i] = rho1 * fact ** (2. / gm1)
+                p[i] = p1 * fact ** (2. * gamma / gm1)
+            elif x < xcd:
+                rho[i] = rho3
+                p[i] = p3
+                u[i] = u3
+            elif x < xsh:
+                rho[i] = rho4
+                p[i] = p4
+                u[i] = u4
+            else:
+                rho[i] = rho5
+                p[i] = p5
+                u[i] = u5
+    else:
+        for i, x in enumerate(x_arr):
+            if x < xsh:
+                rho[i] = rho5
+                p[i] = p5
+                u[i] = -u1
+            elif x < xcd:
+                rho[i] = rho4
+                p[i] = p4
+                u[i] = -u4
+            elif x < xft:
+                rho[i] = rho3
+                p[i] = p3
+                u[i] = -u3
+            elif x < xhd:
+                u[i] = -2. / gp1 * (c1 + (xi - x) / t)
+                fact = 1. + 0.5 * gm1 * u[i] / c1
+                rho[i] = rho1 * fact ** (2. / gm1)
+                p[i] = p1 * fact ** (2. * gamma / gm1)
+            else:
+                rho[i] = rho1
+                p[i] = p1
+                u[i] = -u1
+
+    return x_arr, p, rho, u
+
+
+def solve(left_state, right_state, geometry, t, gamma=1.4, npts=500):
+    """
+    Solves the Sod shock tube problem (i.e. riemann problem) of discontinuity across an interface.
+
+    :rtype : tuple
+    :param left_state: tuple (pl, rhol, ul)
+    :param right_state: tuple (pr, rhor, ur)
+    :param geometry: tuple (xl, xr, xi): xl - left boundary, xr - right boundary, xi - initial discontinuity
+    :param t: time for which the states have to be calculated
+    :param gamma: ideal gas constant, default is air: 1.4
+    :param npts: number of points for array of pressure, density and velocity
+    :return: tuple of: dicts of positions,
+    constant pressure, density and velocity states in distinct regions,
+    arrays of pressure, density and velocity in domain bounded by xl, xr
+    """
+
+    pl, rhol, ul = left_state
+    pr, rhor, ur = right_state
+    xl, xr, xi = geometry
+
+    # basic checking
+    if xl >= xr:
+        print('xl has to be less than xr!')
+        exit()
+    if xi >= xr or xi <= xl:
+        print('xi has in between xl and xr!')
+        exit()
+
+    # calculate regions
+    region1, region3, region4, region5, w = \
+        calculate_regions(pl, ul, rhol, pr, ur, rhor, gamma)
+
+    regions = region_states(pl, pr, region1, region3, region4, region5)
+
+    # calculate positions
+    x_positions = calc_positions(pl, pr, region1, region3, w, xi, t, gamma)
+
+    pos_description = ('Head of Rarefaction', 'Foot of Rarefaction',
+                       'Contact Discontinuity', 'Shock')
+    positions = dict(zip(pos_description, x_positions))
+
+    # create arrays
+    x, p, rho, u = create_arrays(pl, pr, xl, xr, x_positions,
+                                 region1, region3, region4, region5,
+                                 npts, gamma, t, xi)
+
+    val_names = ('x', 'p', 'rho', 'u')
+    val_dict = dict(zip(val_names, (x, p, rho, u)))
+
+    return positions, regions, val_dict
